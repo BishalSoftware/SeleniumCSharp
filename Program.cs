a8d97012ea52df26f29d8cd816b476767cd1a69f
Bishal <bishal.pradhan314@gmail.com>
2020-09-30 08:09:34 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the reference for our browser
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to Google Page
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            driver.Manage().Window.Maximize();
+
+            //Find the Element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform Operation
+            element.SendKeys("selenium with C#");
+
+            element.SendKeys(Keys.Enter);
+        }
+    }
+}
