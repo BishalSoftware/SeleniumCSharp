68060f0fb6cf3364d0b4233ae385a214dfd3b545
Bishal <bishal.pradhan314@gmail.com>
2020-09-30 11:54:15 +0545

    Testing started on C# Program.cs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,52 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SeleniumCSharp
 {
     class Program
     {
+        //Create the reference for our browser
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
         {
-            //Create the reference for our browser
-            IWebDriver driver = new ChromeDriver();
-
-            //Navigate to Google Page
-            driver.Navigate().GoToUrl("https://www.google.com/");
-
-            driver.Manage().Window.Maximize();
-
-            //Find the Element
-            IWebElement element = driver.FindElement(By.Name("q"));
-
-            //Perform Operation
-            element.SendKeys("selenium with C#");
-
-            //search for keywords in google by hitting enter command
-            element.SendKeys(Keys.Enter);
         }
+
+            [SetUp]
+             public void Initialize()
+            {
+                //Navigate to Google Page
+                driver.Navigate().GoToUrl("https://www.google.com/");
+
+                driver.Manage().Window.Maximize();
+            }
+
+            [Test]
+            public void ExecuteTest()
+            {
+                //Find the Element
+                IWebElement element = driver.FindElement(By.Name("q"));
+
+                //Perform Operation
+                element.SendKeys("selenium with C#");
+
+                //search for keywords in google by hitting 'enter' command
+                element.SendKeys(Keys.Enter);
+            }
+
+            [TearDown]
+            public void CleanUp()
+            {
+                Thread.Sleep(2000);
+                driver.Close();
+            }       
     }
 }
